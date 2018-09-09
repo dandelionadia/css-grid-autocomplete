5c5d566493293e48e7531c71c020e470536d1132
dandelionadia <nadia.ridko@gmail.com>
2018-09-09 16:04:58 +0200

    adds get hints js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,28 @@
-$(document).ready(function () {
-  // Create some JavaScript here
+var cities = ['Prague', 'Brno', 'Ostrava', 'Liberec', 'Olomouc', 'Pradubice', 'Jihlava', 'Oprava', 'Most', 'Karlovy Vary'];
+
+var search = document.getElementById('search');
+
+function checkNotEmpty(i) {
+  var isFull = false;
+
+  if(i != '') {
+    isFull = true;
+  } else {
+    isFull = false;
+  }
+  return isFull;
+}
+
+search.addEventListener('input', function (evt) { 
+  console.log(checkNotEmpty(this.value));
 });
+
+function getHints(value, allHints) {
+  var res = [];
+
+  res = cities.filter(city => city[0].toLowerCase() === value.toLowerCase());
+
+  return res;
+}
+
+console.log(getHints('p', cities));
